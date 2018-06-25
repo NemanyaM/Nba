e548c4608e6d8521b1dbe52be31ad4b409e1d8b4
Nemanja Milosavljevic <n.milosavljevic@rent24.com>
2018-06-25 12:06:17 +0200

    All Crud methods done

--- src/main/java/com/nemanja/Dao/MySqlPlayerDaoImplementation.java
+++ src/main/java/com/nemanja/Dao/MySqlPlayerDaoImplementation.java
@@ -17,40 +17,52 @@ public class MySqlPlayerDaoImplementation implements IPlayer{
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    private static class PlayerRowMapper implements RowMapper<Player>{
+        @Override
+        public Player mapRow(ResultSet resultSet, int i) throws SQLException {
+            Player player = new Player();
+            player.setId(resultSet.getInt("id"));
+            player.setName(resultSet.getString("name"));
+            player.setTeam(resultSet.getString("team"));
+            return player;
+        }
+    }
+
     @Override
     public Collection<Player> getAllPlayers() {
 
         final String sql = "SELECT * FROM players";
-        List<Player> players = jdbcTemplate.query(sql, new RowMapper<Player>() {
-            @Override
-            public Player mapRow(ResultSet resultSet, int i) throws SQLException {
-                Player player = new Player();
-                player.setId(resultSet.getInt("id"));
-                player.setName(resultSet.getString("name"));
-                player.setTeam(resultSet.getString("team"));
-                return player;
-            }
-        });
+        List<Player> players = jdbcTemplate.query(sql, new PlayerRowMapper());
         return players;
     }
 
     @Override
     public Player getPlayerById(int id) {
-        return null;
+        final String sql = "SELECT * FROM players where id = ?";
+        Player player = jdbcTemplate.queryForObject(sql, new PlayerRowMapper(), id);
+        return player;
     }
 
     @Override
     public void removePlayerById(int id) {
-
+        final String sql = "DELETE FROM players WHERE id = ?";
+        jdbcTemplate.update(sql, id);
     }
 
     @Override
     public void updatePlayer(Player player) {
-
+        final String sql = "UPDATE players SET name = ?, team = ? WHERE id = ?";
+        int id = player.getId();
+        final String name = player.getName();
+        final String team = player.getTeam();
+        jdbcTemplate.update(sql, new Object[] { name, team, id});
     }
 
     @Override
     public void insertPlayerToDb(Player player) {
-
+        final String sql = "INSERT INTO players (name, team) VALUES (?, ?)";
+        final String name = player.getName();
+        final String team = player.getTeam();
+        jdbcTemplate.update(sql, new Object[] { name, team });
     }
 }
